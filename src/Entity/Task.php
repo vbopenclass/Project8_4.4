6e19c4123762bbcf5da70d85bb21e324fcbb1a47
vbopenclass <vbopenclass@gmail.com>
2020-02-26 12:00:38 +0100

    Namespace correction

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -41,7 +41,7 @@ class Task
     private $isDone;
 
     /**
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="tasks")
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="tasks")
      */
     private $author;
 
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -42,7 +42,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Task", mappedBy="author")
+     * @ORM\OneToMany(targetEntity="App\Entity\Task", mappedBy="author")
      */
     private $tasks;
 
